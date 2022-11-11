5c63615bb8f1bc47361a591aebc0595e798490b5
ftyler <ftyler.ctr@kr.af.mil>
2022-11-11 14:00:02 -0500

    Initial project setup.

---
+++ freelancer-management/src/main/java/com/example/freelancermanagement/FreelancerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.freelancermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerManagementApplication.class, args);
+	}
+
+}
---
+++ freelancer-management/src/test/java/com/example/freelancermanagement/FreelancerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelancermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
